17c757049cf6d8ca74bf50f46339f2bfd7d90184
crellee <crellecrelle@gmail.com>
2016-10-11 10:42:14 +0200

    first commit - empty project

---
+++ This_Is_Soccer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="This_Is_Soccer.MvcApplication" Language="C#" %>
---
+++ This_Is_Soccer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(This_Is_Soccer.Startup))]
+namespace This_Is_Soccer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
