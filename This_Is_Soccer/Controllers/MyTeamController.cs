1537d44385afbf0f0e19a821abb90f0a97cceaf0
crellee <crellecrelle@gmail.com>
2016-11-02 14:04:03 +0100

    the great if sentence working with db. Good stuff in progress

--- This_Is_Soccer/Controllers/MyTeamController.cs
+++ This_Is_Soccer/Controllers/MyTeamController.cs
@@ -18,12 +18,24 @@ namespace This_Is_Soccer.Controllers
         public ActionResult Index()
         {
             string UserId = System.Web.HttpContext.Current.User.Identity.GetUserId();
-            //var myTeam = db.MyTeamModels.Include(m => m.PositionId).ToList();
-            //var query = db.MyTeamModels.Include(m => m.Id).Include(m => m.PositionId).Include(m => m.PlayerId);
-            MyTeamModel myTeamModel = db.MyTeamModels.Find(UserId);
-            return View(myTeamModel);
-            //string UserId = System.Web.HttpContext.Current.User.Identity.GetUserId();
-            //System.Diagnostics.Debug.WriteLine(UserId);
+            /*
+                        var players = from m in db.MyTeamModels select m;
+
+
+                            players = players.Where(s => s.Id.Contains(UserId));
+
+                        //var myTeam = db.MyTeamModels.Include(m => m.PositionId).ToList();
+                        //var query = db.MyTeamModels.Include(m => m.Id).Include(m => m.PositionId).Include(m => m.PlayerId);
+                        //MyTeamModel myTeamModel = db.MyTeamModels.Find(UserId);
+                        return View(players);
+                        //string UserId = System.Web.HttpContext.Current.User.Identity.GetUserId();
+                        //System.Diagnostics.Debug.WriteLine(UserId);
+                        */
+
+
+
+            var myTeamModels = db.MyTeamModels.Include(m => m.Player).Where(m => m.Id.Contains(UserId));
+            return View(myTeamModels.ToList());
         }
     }
 }
