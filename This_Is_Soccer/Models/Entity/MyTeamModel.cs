4cc51a9a983e6f54ad256bb220cb771069851664
crellee <crellecrelle@gmail.com>
2016-11-02 14:49:40 +0100

    model and view updated myteam

--- This_Is_Soccer/Models/Entity/MyTeamModel.cs
+++ This_Is_Soccer/Models/Entity/MyTeamModel.cs
@@ -9,19 +9,13 @@ namespace This_Is_Soccer.Models.Entity
 {
     public class MyTeamModel
     {
-        /*
+        
         [Key]
-        [Column(Order = 0)]
         public string MyTeamId { get; set; }
-        */
 
-        [Key]
-        [Column(Order = 1)]
         public string Id { get; set; }
         public ApplicationUser User { get; set; }
 
-        [Column(Order = 2)]
-        public int PositionId { get; set; }
         public int PlayerId { get; set; }
         public PlayerModel Player { get; set; }
         
