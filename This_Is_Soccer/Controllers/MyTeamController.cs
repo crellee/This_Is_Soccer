355f0a71dc74e77512450ce137ce338bf97623b2
crellee <crellecrelle@gmail.com>
2016-11-02 11:36:11 +0100

    my team controller added

---
+++ This_Is_Soccer/Controllers/MyTeamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace This_Is_Soccer.Controllers
+{
+    public class MyTeamController : Controller
+    {
+        // GET: MyTeam
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
