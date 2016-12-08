b3ee6461834668198d420bf6479239b956f09151
crellee <crellecrelle@gmail.com>
2016-12-08 13:29:12 +0100

    test is trying

---
+++ This_Is_Soccer.tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using This_Is_Soccer;
+
+namespace This_Is_Soccer.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mocrepo2 = new Mock<IGenericRepository>();
+
+        }
+    }
+}
