439633684e5bd0128a2a69578499062033c8c06d
crellee <crellecrelle@gmail.com>
2016-12-08 14:08:41 +0100

    test is trying again

--- This_Is_Soccer.tests/UnitTest1.cs
+++ This_Is_Soccer.tests/UnitTest1.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using This_Is_Soccer;
+using This_Is_Soccer.Models.Interface;
+using This_Is_Soccer.Models;
+using This_Is_Soccer.Models.Entity;
+using This_Is_Soccer.Controllers;
 
 namespace This_Is_Soccer.tests
 {
@@ -11,7 +14,16 @@ namespace This_Is_Soccer.tests
         [TestMethod]
         public void TestMethod1()
         {
-            var mocrepo2 = new Mock<IGenericRepository>();
+            var mocrepo2 = new Mock<IGenericRepository<PlayerModel>>();
+            PlayerController controller = new PlayerController(mocrepo2.Object);
+            PlayerModel player = new PlayerModel()
+            {
+                PlayerId = 200,
+                PlayerName = "Chris",
+                PlayerPic = "http://futhead.cursecdn.com/static/img/17/players/193041.png",
+                ClubId = 2,
+                PositionId = 1
+            };
 
         }
     }
--- This_Is_Soccer/Models/Interface/IGenericRepository.cs
+++ This_Is_Soccer/Models/Interface/IGenericRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace This_Is_Soccer.Models.Interface
 {
-    interface IGenericRepository<T> where T:class
+    public interface IGenericRepository<T> where T:class
     {
         IEnumerable<T> SelectAll();
         T SelectByID(object id);
